682e9a6ebe03bb59e42a862c3c6ea71bc6b2b7b2
joelmorera <joel.joan.morera@estudiantat.upc.edu>
2023-03-16 19:20:09 +0100

    Add index.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,45 @@
-// Load Wi-Fi library
 #include <WiFi.h>
+#include <WebServer.h>
 
-// HTML & CSS contents which display on web server
-String HTML = " <!DOCTYPE html>\
-                <html>\
-                    <body>\
-                        <h1>My Primera Pagina con ESP32 - AP Mode &#128522;</h1>\
-                    </body>\
-                </html>";
+extern String HTML;
 
-// Replace with your network credentials
-const char* ssid     = "ESP32-Access-Point";
-const char* password = "123456789";
+// SSID & Password
+const char* ssid = "Xiaomi_11T_Pro"; // Enter your SSID here
+const char* password = "f5cbd8a82232"; //Enter your Password here
 
-// Set web server port number to 80
-WiFiServer server(80);
+WebServer server(80);// Object of WebServer(HTTP port, 80 is default)
 
-String header;
+void handle_root();
 
-void setup() {
-  Serial.begin(115200);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Setting AP (Access Point) ...");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
-
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-  
-  server.begin();
+void setup() 
+{
+    Serial.begin(115200);
+    Serial.print("Try Connecting to ");
+    Serial.println(ssid);
+    // Connect to your wi-fi modem
+    WiFi.begin(ssid, password);
+    // Check wi-fi is connected to wi-fi network
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected successfully");
+    Serial.print("Got IP: ");
+    Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
+    server.on("/", handle_root);
+    server.begin();
+    Serial.println("HTTP server started");
+    delay(100);
 }
 
 void loop() 
 {
-  WiFiClient client = server.available();   // Listen for incoming clients
+    server.handleClient();
+}
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {                    // if the byte is a newline character
-        char c = client.read();
-        Serial.write(c);
-        header += c;
-        /*if(c == '\n')
-        {*/
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // Display the HTML web page
-            client.println(HTML);
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } 
-          else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        //}
-        /*else if(c == '\r')
-        {
-          currentLine +=c;
-        }*/
-      }
-    }
-
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void handle_root() 
+{
+    server.send(200, "text/html", HTML);
 }
